fedf0692b8aeb76e2d8e21e9ea2fe0e29a5c68f6
Israel Lira <ilira@norwich.edu>
2019-08-29 08:01:51 -0400

    First Commit!

--- prog1/main.cpp
+++ prog1/main.cpp
@@ -6,18 +6,20 @@
 using namespace std;
 
 int main() {
-    cout << "Creating a character array of size 5." << endl;
+    cout << "Creating a character array of size 7." << endl;
 
-    myArray newArr(5);
-    newArr.SetArray('M', 0);
-    newArr.SetArray('a', 1);
-    newArr.SetArray('t', 2);
-    newArr.SetArray('t', 3);
-    newArr.SetArray('!', 4);
+    myArray newArr(7);
+    newArr.SetArray('I', 0);
+    newArr.SetArray('s', 1);
+    newArr.SetArray('r', 2);
+    newArr.SetArray('a', 3);
+    newArr.SetArray('e', 4);
+    newArr.SetArray('l', 5);
+    newArr.SetArray('!', 6);
 
     cout << "Array contents: " << newArr.GetArray(0) <<
             newArr.GetArray(1) << newArr.GetArray(2) <<
-            newArr.GetArray(3) << newArr.GetArray(4) << endl;
+            newArr.GetArray(3) << newArr.GetArray(4) << newArr.GetArray(5) << newArr.GetArray(6) << endl;
 
     return 0;
 }
