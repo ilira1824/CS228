25ed52861e035a84aa48dd674b1f2e47d0ef3be1
Israel Lira <Israel Lira@DESKTOP-PB7LJH7>
2019-08-29 16:32:58 -0400

    Second commit from second device.

---
+++ ilira1824-cs-228-a54ae36cf953/ilira1824-cs-228-a54ae36cf953/prog1/main.cpp
@@ -0,0 +1,35 @@
+// main.cpp
+
+#include <iostream>
+#include "../array.h"
+
+using namespace std;
+
+int main() {
+    cout << "Creating a character array of size 7." << endl;
+
+    myArray newArr(12);
+    newArr.SetArray('I', 0);
+    newArr.SetArray('s', 1);
+    newArr.SetArray('r', 2);
+    newArr.SetArray('a', 3);
+    newArr.SetArray('e', 4);
+    newArr.SetArray('l', 5);
+    newArr.SetArray(' ', 6);
+    newArr.SetArray('L', 7);
+    newArr.SetArray('i', 8);
+    newArr.SetArray('r', 9);
+    newArr.SetArray('a', 10);
+    newArr.SetArray('!', 11);
+
+    cout << "Array contents: " << newArr.GetArray(0) <<
+            newArr.GetArray(1) << newArr.GetArray(2) <<
+            newArr.GetArray(3) << newArr.GetArray(4) << 
+            newArr.GetArray(5) << newArr.GetArray(6) <<
+            newArr.GetArray(7) << newArr.GetArray(8) << 
+            newArr.GetArray(9) << newArr.GetArray(10) << 
+            newArr.GetArray(11) << endl;
+
+    return 0;
+}
+
