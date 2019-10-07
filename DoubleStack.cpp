8b4fe7ade4e1cf64867e3aa1cb6b1aef07515e58
Israel Lira <ilira@norwich.edu>
2019-10-06 23:11:11 -0400

    Now has skeleton code of Stack Assignment

---
+++ DoubleStack.cpp
@@ -0,0 +1,50 @@
+// DoubleStack.cpp
+
+#include "DoubleStack.h"
+#include <iostream>
+
+// Default constructor, the stack is empty to start
+DoubleStack::DoubleStack() {
+// Insert your code here 
+}
+
+
+// Default destructor
+DoubleStack::~DoubleStack() {
+// Insert your code here 
+}
+
+// Add "value" to the top of stack A
+void DoubleStack::PushA(char value) {
+// Insert your code here 
+}
+
+// Add "value" to the top of stack B
+void DoubleStack::PushB(char value) {
+// Insert your code here 
+}
+
+// Remove and return the item on the top of stack A
+char DoubleStack::PopA() {
+// Insert your code here 
+}
+
+// Remove and return the item on the top of stack B
+char DoubleStack::PopB() {
+// Insert your code here 
+}
+
+// Return the item on the top of stack A
+char DoubleStack::TopA() {
+// Insert your code here 
+}
+
+// Return the item on the top of stack B
+char DoubleStack::TopB() {
+// Insert your code here 
+}
+
+// Return the number of items in the stack
+unsigned int DoubleStack::size() {
+// Insert your code here 
+}
