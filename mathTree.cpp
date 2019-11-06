81992c03db24a7d0db2d0da7a35621994d5a1551
Israel Lira <ilira@norwich.edu>
2019-11-06 18:04:57 -0500

    Added code for _evaluate and _inorder

--- mathTree.cpp
+++ mathTree.cpp
@@ -12,42 +12,79 @@ string stringify(long double x) {
     return o.str();
 }
 
+// Default constructor
+
 mathTree::mathTree() {
-// Add content here
 }
+
+// Clean up any dynamically allocated data
 
 mathTree::~mathTree() {
-// Add content here
+
 }
+
+// Reads an expression in prefix notation and builds a tree appropriately
 
 void mathTree::ReadExpression(string s) {
-// Reads an expression in prefix notation and builds a tree appropriately
+    root = new treeNode;
+    _readex(root,s);
 }
 
+// Populates the node t (which has already been allocated) with the prefix expression exp
 
 string mathTree::_readex(treeNode* t, string exp) {
-// Populates the node t (which has already been allocated) with the prefix expression exp
+
 }
+
+// Returns the result of evaluate() on the root of the tree
 
 double mathTree::ExpressionValue() {
-// Returns the result of evaluate() on the root of the tree
+    return _evaluate(root);
 }
+
+// Returns the value of the expression rooted at t
 
 double mathTree::_evaluate(treeNode* t) {
-// Returns the value of the expression rooted at t
+    if (t == NULL)
+        return 0;
+    
+    int lval, rval;
+    
+    if (t->leafNode == true)
+        return t->value;
+    
+    lval = _evaluate(t->lChild);
+    rval = _evaluate(t->rChild);
+    
+    if (t->op == '*')
+        return lval * rval;
+    else if (t->op == '+')
+        return lval + rval;
+    else
+        return "invalid";
 }
 
-string mathTree::ReturnInfix() {
 // Traverses the tree, printing out an infix version of the expression, with parentheses around each expression
+
+string mathTree::ReturnInfix() {
+    return _inorder(root);
 }
+
+// Perform an inorder traversal of the tree starting at t
 
 string mathTree::_inorder(treeNode* t)
 {
-    if (t == NULL)
-        return;
-    _inorder(t->lChild);
-    cout << t->value << endl;
-    _inorder(t->rChild);
-    cout << t->rChild << endl;
-// Add content here  
+    if (t == NULL)                  // Base case: No recursion will occur if parent node has no children (leaf node)
+
+    string s;
+    
+    if (t->leafNode == true)
+        s += stringify(t->value);
+    else {
+        s += _inorder(t->lChild);
+        s += t->op;
+        s += _inorder(t->rChild);
+    }
+    
+    return s;
 }
